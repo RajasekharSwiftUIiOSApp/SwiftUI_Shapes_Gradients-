7209381411b590c6e749061597d56012147192dd
Raja <Rajasekhar.iosswift@gmail.com>
2024-06-07 18:38:06 +0530

    Initial Commit

---
+++ SwiftUI_Shapes_Gradients/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Shapes_Gradients
+//
+//  Created by rajasekhar pidikiti on 07/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Shapes_Gradients/SwiftUI_Shapes_GradientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Shapes_GradientsApp.swift
+//  SwiftUI_Shapes_Gradients
+//
+//  Created by rajasekhar pidikiti on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Shapes_GradientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
